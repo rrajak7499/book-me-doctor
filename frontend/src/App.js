5b711fe42efbadc27f0c9b8ff9bd01a820ddd4db
rrajak7499 <rrajak7499@gmail.com>
2022-09-17 20:42:13 +0530

    made changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Start 16/09/2022 Hello world</div>;
+  return (
+    <>
+      <div className="flex bg-gray-700 min-h-screen justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <h1 className="text-6xl">Bhai Itna he hua hai 😥</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
