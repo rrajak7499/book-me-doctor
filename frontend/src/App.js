8d641837dca1d3e2f7727891b252e8bc78dbc48a
rrajak7499 <rrajak7499@gmail.com>
2022-09-17 20:20:02 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Start 16/09/2022
-    </div>
-  )
+  return <div className="App">Start 16/09/2022 Hello world</div>;
 }
 
 export default App;
