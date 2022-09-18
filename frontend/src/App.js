6d90dc1572512de5e18493a7480d7037a85b2256
Abhishek kumar <aky8507049610@gmail.com>
2022-09-18 22:47:28 +0530

    project folder structure is ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <div className="flex bg-gray-700 min-h-screen justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-        <h1 className="text-6xl">Bhai Itna he hua hai 😥</h1>
+        {/* <h1 className="text-6xl">Bhai Itna he hua hai 😥</h1> */}
+        <Navbar />
       </div>
     </>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navbar />
+    {/* <Navbar /> */}
     <App />
   </React.StrictMode>
 );
