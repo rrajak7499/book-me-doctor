1cff8eeeceef2b9befb41101953fb955cc74da9b
Abhishek kumar <aky8507049610@gmail.com>
2022-09-19 14:16:24 +0530

    sir discussion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,21 @@ import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <>
-      <div className="flex bg-gray-700 min-h-screen justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-        {/* <h1 className="text-6xl">Bhai Itna he hua hai 😥</h1> */}
+      <div className="flex bg-gray-700  justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        {/* <div className="h-16 bg-red-400">
+          Categories
+        </div> */}
         <Navbar />
       </div>
+      <div className="flex justify-center items-center mt-16 h-20 bg-red-400">
+          <h1 className='text-2xl text-white'>Categories</h1>
+        </div>
+        <div className="flex justify-center items-center mt-1 h-36 p-4 bg-blue-600">
+          <h1 className='text-2xl text-white'>Carosal</h1>
+        </div>
+        <div className="flex min-h-screen justify-center items-center mt-1 h-32 bg-red-400">
+          <h1 className='text-2xl text-white'>All Data</h1>
+        </div>
     </>
   );
 }
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,15 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
+
 const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
+  name: "Kartik Jha",
+  email: "kartikJha123@gmail.com",
   imageUrl:
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
+
+// Navigation option
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
   { name: "Team", href: "#", current: false },
@@ -15,11 +18,14 @@ const navigation = [
   { name: "Calendar", href: "#", current: false },
   { name: "Reports", href: "#", current: false },
 ];
+
+// Profile option
 const userNavigation = [
   { name: "Your Profile", href: "#" },
   { name: "Settings", href: "#" },
   { name: "Sign out", href: "#" },
 ];
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -35,14 +41,22 @@ export default function Navbar() {
               <div className="mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
+                    <div className="flex flex-col">
                     <div className="flex-shrink-0">
-                      <img
+                      {/* <img
                         className="h-8 w-8"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                         alt="Your Company"
-                      />
+                      /> */}
+                      <h1 className="text-xl text-white">Book Any Doctor</h1>
+                    </div>
+                    <div className="">
+                      <h1 className="text-white text-sm">🏮 Slect Location</h1>
+                    </div>
                     </div>
                   </div>
+
+                  {/* Navigation option in desktop*/}
                   <div className="hidden md:block">
                     <div className="ml-10 flex items-baseline space-x-4">
                       {navigation.map((item) => (
@@ -62,6 +76,18 @@ export default function Navbar() {
                       ))}
                     </div>
                   </div>
+
+
+                  <label class="relative block">
+  <span class="sr-only">Search</span>
+  <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+    <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
+  </span>
+  <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+</label>
+
+
+
 
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
@@ -87,13 +113,15 @@ export default function Navbar() {
                         </div>
                         <Transition
                           as={Fragment}
-                          enter="transition ease-out duration-100"
+                          enter="transition ease-out duration-400"
                           enterFrom="transform opacity-0 scale-95"
                           enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
+                          leave="transition ease-in duration-200"
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
                         >
+
+                          {/* dropdown options */}
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             {userNavigation.map((item) => (
                               <Menu.Item key={item.name}>
@@ -115,8 +143,9 @@ export default function Navbar() {
                       </Menu>
                     </div>
                   </div>
+
+                   {/* Mobile menu button */}
                   <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
                     <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
@@ -135,6 +164,8 @@ export default function Navbar() {
                 </div>
               </div>
 
+
+              {/* Mobile View start here */}
               <Disclosure.Panel className="md:hidden">
                 <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                   {navigation.map((item) => (
@@ -156,12 +187,17 @@ export default function Navbar() {
                 </div>
                 <div className="border-t border-gray-700 pt-4 pb-3">
                   <div className="flex items-center px-5">
+                    <div className="flex">
                     <div className="flex-shrink-0">
                       <img
                         className="h-10 w-10 rounded-full"
                         src={user.imageUrl}
                         alt=""
                       />
+                    </div>
+                    <div className="">
+                      <h1 className="text-white">🏮 Slect Location</h1>
+                    </div>
                     </div>
                     <div className="ml-3">
                       <div className="text-base font-medium leading-none text-white">
